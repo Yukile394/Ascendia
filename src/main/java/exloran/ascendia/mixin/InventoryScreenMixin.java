86856981292e4fc8cbd6c088c5aa13047c41cec8
Yukile394 <muhamedmuhet@gmail.com>
2026-06-25 06:24:33 +0300

    Refactor InventoryScreenMixin to use accessor methods

--- src/main/java/exloran/ascendia/mixin/InventoryScreenMixin.java
+++ src/main/java/exloran/ascendia/mixin/InventoryScreenMixin.java
@@ -11,16 +11,12 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InventoryScreen.class)
 public abstract class InventoryScreenMixin extends HandledScreen<PlayerScreenHandler> {
-
-    @Shadow protected int backgroundWidth;
-    @Shadow protected int backgroundHeight;
 
     public InventoryScreenMixin(PlayerScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -30,13 +26,18 @@ public abstract class InventoryScreenMixin extends HandledScreen<PlayerScreenHan
     private void ascendia$addButtons(CallbackInfo ci) {
         AscendiaConfig cfg = AscendiaClient.CONFIG;
 
+        HandledScreenAccessor acc = (HandledScreenAccessor) this;
+        int bgX = acc.ascendia$getX();
+        int bgY = acc.ascendia$getY();
+        int bgW = acc.ascendia$getBackgroundWidth();
+        int bgH = acc.ascendia$getBackgroundHeight();
+
         final int btnWidth = 70;
         final int btnHeight = 16;
         final int spacing = 4;
         final int totalWidth = btnWidth * 3 + spacing * 2;
-
-        final int startX = this.getX() + this.backgroundWidth / 2 - totalWidth / 2;
-        final int rowY = this.getY() + this.backgroundHeight + 4;
+        final int startX = bgX + bgW / 2 - totalWidth / 2;
+        final int rowY = bgY + bgH + 4;
 
         AscendiaButton editBtn = AscendiaButton.create(
                 startX, rowY, btnWidth, btnHeight,
